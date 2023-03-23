36542b33d6041a239c5288c388b164538ff50f07
Zoard Ehmann <zoard.ehmann@gmail.com>
2023-03-23 16:05:31 +0100

    fix: Make repeat string test work

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, nrOfTimes) {
+  let result = "";
+  if (nrOfTimes < 0) {
+    result = "ERROR";
+  } else {
+    for (let i = 0; i < nrOfTimes; i++) result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
