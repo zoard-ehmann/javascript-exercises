c1a7bf09ff7c3892e9e678e7b365a8ccc570c50a
Zoard Ehmann <zoard.ehmann@gmail.com>
2023-03-23 16:52:43 +0100

    fix: Make remove from array test work

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  const arr = array;
 
+  for (let arg of args) {
+    while (arr.indexOf(arg) !== -1) {
+      arr.splice(arr.indexOf(arg), 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
