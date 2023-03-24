7f89a459e75cb847d60b1cb7013b59ca4ef1d9b1
Zoard Ehmann <zoard.ehmann@gmail.com>
2023-03-24 09:49:41 +0100

    fix: Make sum all test work

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const validNumber = (num) => {
+  return typeof num === "number" && num >= 0 ? true : false;
+};
 
+const sumAll = function (x, y) {
+  if (!validNumber(x) || !validNumber(y) || x === y) return "ERROR";
+
+  let start, end;
+  if (x < y) {
+    start = x;
+    end = y;
+  } else {
+    start = y;
+    end = x;
+  }
+
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
