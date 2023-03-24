42dc251c950fb335c05960c608b47c21aae1c036
Zoard Ehmann <zoard.ehmann@gmail.com>
2023-03-24 10:12:20 +0100

    fix: Make leap years test work

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let isLeap = false;
+  if (year % 4 === 0) {
+    isLeap = true;
+    if (year % 100 === 0) {
+      isLeap = year % 400 === 0 ? true : false;
+    }
+  }
+  return isLeap;
 };
 
 // Do not edit below this line
