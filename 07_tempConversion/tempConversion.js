66bb9e0f7f9e06c1bde0a209177f2113e78615bf
Zoard Ehmann <zoard.ehmann@gmail.com>
2023-03-24 10:32:26 +0100

    fix: Make temp conversion test work

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const roundOneDec = (num) => {
+  return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (f) {
+  return roundOneDec((f - 32) * (5 / 9));
+};
+
+const convertToFahrenheit = function (c) {
+  return roundOneDec(c * (9 / 5) + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
